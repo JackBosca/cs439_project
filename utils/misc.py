28c7f36e249ff867ced08f0071f79df31e01526d
JackBosca <boscarioljacopo@gmail.com>
2025-06-07 23:24:34 +0200

    release of miscellaneous module

---
+++ utils/misc.py
@@ -0,0 +1,74 @@
+import os
+import random
+import numpy as np
+import torch
+from torch.utils.data import DataLoader
+from data.preprocessing import load_and_preprocess_data
+from data.datasets import TextDataset
+from models.models_utils import get_tokenizer
+
+# Set seed
+def set_seed(seed=42):
+    # Python built-ins
+    random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+
+    # NumPy
+    np.random.seed(seed)
+
+    # PyTorch
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)  # if using multi-GPU
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+def create_loaders(config, batch_size=4, shuffle=False):
+    """
+    Create data loaders for training, validation, and test datasets.
+    Args:
+        config: Configuration object containing model and data parameters.
+        batch_size (int): Batch size for the data loaders.
+        shuffle (bool): Whether to shuffle the training data.
+    Returns:
+        train_loader: DataLoader for the training dataset.
+        val_loader: DataLoader for the validation dataset.
+        test_loader: DataLoader for the test dataset.
+    """
+    # Load data
+    train_texts, val_texts, test_texts = load_and_preprocess_data()
+    
+    # Get tokenizer
+    tokenizer, _ = get_tokenizer(config.model_name)
+
+    # Create datasets
+    train_dataset = TextDataset(train_texts, tokenizer, config.max_length)
+    val_dataset = TextDataset(val_texts, tokenizer, config.max_length)
+    test_dataset = TextDataset(test_texts, tokenizer, config.max_length)
+    
+    # Create dataloaders
+    val_loader = DataLoader(
+        val_dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        num_workers=config.num_workers,
+        pin_memory=config.pin_memory
+    )
+
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        num_workers=config.num_workers,
+        pin_memory=config.pin_memory
+    )
+
+    test_loader = DataLoader(
+        test_dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        num_workers=config.num_workers,
+        pin_memory=config.pin_memory
+    )
+    
+    return train_loader, val_loader, test_loader
