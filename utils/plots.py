724f592bf91fcd8b11504eb58c23aabf4bc6e2e8
JackBosca <boscarioljacopo@gmail.com>
2025-06-07 18:57:41 +0200

    update learning curve function

--- utils/plots.py
+++ utils/plots.py
@@ -1,9 +1,11 @@
+import os
 import matplotlib.pyplot as plt
 
-def plot_loss_curves(results_list):
+def plot_loss_curves(results_list, save_path=None, fname=None):
     plt.figure(figsize=(12, 6))
+
     for results in results_list:
-        label = f"{results['optimizer']}, lr={results['lr']}, bs={results['batch_size']}"
+        label = f"{results['optimizer']}, lr={results['lr']}, wd={results['weight_decay']}, bs={results['batch_size']}"
         plt.plot(results['train_loss_history'], label=f"Train - {label}")
         plt.plot(results['val_loss_history'], '--', label=f"Val - {label}")
     
@@ -12,4 +14,12 @@ def plot_loss_curves(results_list):
     plt.title("Training and Validation Loss Curves")
     plt.legend()
     plt.grid()
+
+    if save_path is not None and fname is not None:
+        os.makedirs(save_path, exist_ok=True)
+        file_name = os.path.join(save_path, f"loss_curves_{fname}.png")
+        plt.savefig(file_name)
+        print(f"Plot saved to: {file_name}")
+
     plt.show()
+    plt.close()
